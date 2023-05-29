a4b0eec84d3dadab82cb4a7e891eee167a8f4887
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-05-30 03:01:07 +0530

    add another partner

--- src/data/partner-data.js
+++ src/data/partner-data.js
@@ -6,6 +6,13 @@ export const partnerData = [
     desc: "Shayna EcoUnified is an ISO 9001 : 2015 certified company incorporated with the intent to reduce the dumping of plastics in the environment, by creating affordable materials and structural products by recycling the plastic waste and thus creating a cleaner tomorrow for the generations to come.",
     link: "https://www.shaynaecounified.com/",
   },
+  {
+    image: "/img/rr-visit.jpg",
+    name: "Ghaziabad Nagar Nigam RRR center",
+    date: "Visit",
+    desc: "A NASA recognized technology innocvator, Attero is India's largest electronic asset management company. As a pioneer in the electronic waste management sector, Attero has been spearheading efforts to tackle the E-waste issue through it's 360 degree approach. Powered by disruptive clean technology, Attero is the only Indian company, and one among seven globally, with the capability to extract valueable materials from E-waste in an eco-friendly manner.",
+    link: "https://www.shaynaecounified.com/",
+  },
 ];
 
 export default partnerData;
