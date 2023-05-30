5e1c74fe25c0e1356854e05959007b43f0f8337b
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-05-30 13:11:23 +0530

    addad photos

--- src/data/partner-data.js
+++ src/data/partner-data.js
@@ -7,7 +7,7 @@ export const partnerData = [
     link: "https://www.shaynaecounified.com/",
   },
   {
-    image: "/img/rr-visit.jpg",
+    image: "/img/IMG-8.jpg",
     name: "Ghaziabad Nagar Nigam RRR center",
     date: "Visit",
     desc: "A NASA recognized technology innocvator, Attero is India's largest electronic asset management company. As a pioneer in the electronic waste management sector, Attero has been spearheading efforts to tackle the E-waste issue through it's 360 degree approach. Powered by disruptive clean technology, Attero is the only Indian company, and one among seven globally, with the capability to extract valueable materials from E-waste in an eco-friendly manner.",
